d2bc975a63bad01e64ec0e2bd8aa55181f90c332
Lucian <42606+gni@users.noreply.github.com>
2018-10-31 21:04:58 +0100

    link :frog:

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <Header />
-    <h1>About you</h1>
+    <Link to="/">Home</Link>
+    <Header headerText="About page" />
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed cras ornare arcu dui vivamus.</p>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default () => (
 <div style={{ color: `green` }}>
-<h1>Hello world!</h1>
+<Link to="/contact/">Contact</Link>
+<Header headerText="Hello world!" />
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 <img src="https://source.unsplash.com/random/400x200" alt="" />
 </div>
