8964000281339eaa5ebb189e9482e6a27ae76739
Lucian <42606+gni@users.noreply.github.com>
2018-10-31 20:40:49 +0100

    header :frog:

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,9 @@
 import React from "react"
+import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
+    <Header />
     <h1>About you</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed cras ornare arcu dui vivamus.</p>
   </div>
