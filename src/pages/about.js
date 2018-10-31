884ffb4c916aa50be4da1e91f264fa85e5794f9b
Lucian <42606+gni@users.noreply.github.com>
2018-10-31 20:36:38 +0100

    about page :frog:

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <h1>About you</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed cras ornare arcu dui vivamus.</p>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,7 @@ import React from "react"
 export default () => (
 <div style={{ color: `green` }}>
 <h1>Hello world!</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+<img src="https://source.unsplash.com/random/400x200" alt="" />
 </div>
 )
